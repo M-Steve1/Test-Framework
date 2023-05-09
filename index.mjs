86dfd22e68553229ef6e30ccc20806d79abad11a
Modebe Stephen <modebestephen2@gmail.com>
2023-05-09 13:05:29 +0100

    Add assertion framework

--- index.mjs
+++ index.mjs
@@ -1,9 +1,9 @@
 import  { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { dirname, join, relative } from 'path';
 import  JestHasteMap  from 'jest-haste-map';
 import { cpus } from 'os';
-import fs from 'fs';
 import { Worker }  from 'jest-worker';
+import chalk from 'chalk';
 
 const root = dirname(fileURLToPath(import.meta.url))
 const hasteMapOptions = {
@@ -18,14 +18,40 @@ const hasteMapOptions = {
 const jestHasteMap = new JestHasteMap.default(hasteMapOptions);
 await jestHasteMap.setupCachePath(hasteMapOptions);
 const { hasteFS } = await jestHasteMap.build();
-const testFiles = hasteFS.matchFilesWithGlob(['**/*.test.js']);
+const testFiles = hasteFS.matchFilesWithGlob([process.argv[2] ? `**/${process.argv[2]}*`: '**/*.test.js']);
 const worker = new Worker(join(root, 'worker'), {
     enableWorkerThread: true
 });
 
+let hasFailed;
+
 for await (const testFile of testFiles) {
-    const code = await worker.runTest(testFile);
-    console.log(code);
+    const { success, testResults, errorMessage } = await worker.runTest(testFile);
+    const status = success ? chalk.green.inverse.bold('PASS') : chalk.red.inverse.bold('FAIL');
+    console.log(`${status} ${chalk.dim(relative(root, testFile))}`);
+
+
+    if(!success) {
+        hasFailed = true;
+        // Make use of the well-detailed `testResults` and error messages
+        if(testResults.length > 0 && testResults !== []) {
+            // Filter the testResults and only gets the test that failed
+            testResults
+            .filter((result) => result.errors.length)
+            .forEach((result) => console.log(
+                result.testPath.slice(1).join(" ") + "\n" + result.errors[0]
+            ));
+            // If it crashes before `jest-circus ran report this`
+        } else if (errorMessage) {
+            console.log(" " + errorMessage);
+        }
+    }
 }
 
+
 worker.end();
+
+if(hasFailed) {
+    console.log(chalk.red.bold('\n Test run failed, fix all failing tests'));
+    process.exitCode = 1 // set exit code if test fails
+}
--- worker.js
+++ worker.js
@@ -1,6 +1,40 @@
 const fs = require('fs');
+const { describe, it, mock, resetState, run } = require('jest-circus');
+const NodeEnvironment = require('jest-environment-node');
+const vm = require('vm');
+const  { expect } = require('expect');
 
 exports.runTest = async (testFile) => {
     const code = await fs.promises.readFile(testFile, 'utf8');
-    return code;
+
+    const testResult = {
+        errorMessage: null,
+        success: false,
+        testResults: []
+    }
+
+    try {
+        resetState();
+        const { testResults } = await run();
+        testResult.testResults = testResults
+        // testResult.success = testResults.every((result) => !result.errors.length);
+        testResult.success = true;
+        const environment = new NodeEnvironment.default({
+            projectConfig: {
+                testEnvironmentOptions: {
+                    expect,
+                    describe,
+                    it,
+                    mock,
+                }
+            }
+        })
+        vm.runInContext(code, environment.getVmContext());
+    
+    } catch (error) {
+        testResult.success = false
+        testResult.errorMessage = error;
+    }
+
+    return testResult;
 }
